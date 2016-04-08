e66e2ab84aeb712ff38c55243e0d28df71f53c65
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2016-04-08 11:31:04 -0600

    Add watch setup for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,9 +19,43 @@ module.exports = function(grunt) {
           ext:'.js'
         }]
       }
+    },
+
+    concurrent: {
+      dev: {
+        tasks: ['connect', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          base: {
+            path: 'public'
+          }
+        }
+      }
+    },
+
+    watch: {
+      js: {
+        files: ['src/javascripts/*.js', 'src/javascripts/**/*.js'],
+        tasks: ['babel'],
+        options: { spawn: false }
+      }
     }
   });
 
+  if (process.env.NODE_ENV !== 'production') {
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+  }
+
   grunt.registerTask('build', ['babel']);
-  grunt.registerTask('default', ['build']);
+  grunt.registerTask('default', ['build', 'concurrent']);
 };
